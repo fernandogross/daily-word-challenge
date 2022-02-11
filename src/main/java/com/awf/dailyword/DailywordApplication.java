77b34eecf81660c2c594d132840133e4d8b9f177
Fernando Gross <fernando.gross@ehi.com>
2022-02-11 00:51:49 -0300

    Added Spring Boot Initializr setup

---
+++ src/main/java/com/awf/dailyword/DailywordApplication.java
@@ -0,0 +1,13 @@
+package com.awf.dailyword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailywordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailywordApplication.class, args);
+	}
+
+}
